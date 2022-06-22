e92694de65620efd7b4bb7f4efd40dfd7a5b4c97
Rithik Jain <rithik.jain3006@gmail.com>
2022-06-23 04:10:39 +0530

    wip: firebase phone auth

--- app/src/main/java/com/dscvit/policewatch/ui/auth/PhoneNumberActivity.kt
+++ app/src/main/java/com/dscvit/policewatch/ui/auth/PhoneNumberActivity.kt
@@ -2,12 +2,29 @@ package com.dscvit.policewatch.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.dscvit.policewatch.databinding.ActivityPhoneNumberBinding
+import com.dscvit.policewatch.ui.utils.LoadingDialog
+import com.dscvit.policewatch.ui.utils.showErrorSnackBar
+import com.dscvit.policewatch.ui.utils.showSuccessSnackBar
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
 
 class PhoneNumberActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "PhoneNumberActivity"
+    }
+
     private lateinit var binding: ActivityPhoneNumberBinding
+    private val loadingDialog by lazy { LoadingDialog(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +37,65 @@ class PhoneNumberActivity : AppCompatActivity() {
 
     private fun setupListeners() {
         binding.nextButton.setOnClickListener {
-            val intent = Intent(this, VerifyOtpActivity::class.java)
-            startActivity(intent)
+            if (binding.mobileNumberEditText.text.length != 10) {
+                binding.root.showErrorSnackBar("Please enter a valid phone number")
+            } else {
+                verifyPhone("+91${binding.mobileNumberEditText.text}")
+                loadingDialog.start("Sending an OTP...")
+            }
         }
+    }
+
+    private fun navigateToVerifyOtpActivity(verificationID: String) {
+        val intent = Intent(this, VerifyOtpActivity::class.java)
+        intent.putExtra("verificationID", verificationID)
+        startActivity(intent)
+    }
+
+    private val phoneAuthCallbacks =
+        object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                signInWithPhoneAuthCredential(credential)
+                loadingDialog.stop()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                loadingDialog.stop()
+                binding.root.showErrorSnackBar("onVerificationFailed: $e")
+                Log.w(TAG, "onVerificationFailed", e)
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                loadingDialog.stop()
+                Log.d(TAG, "onCodeSent:$verificationId")
+                navigateToVerifyOtpActivity(verificationId)
+            }
+        }
+
+    private fun verifyPhone(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(Firebase.auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(90L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(phoneAuthCallbacks)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        Firebase.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    binding.root.showSuccessSnackBar("Signed In!")
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user
+                } else {
+                    binding.root.showErrorSnackBar("signInWithCredential:failure - ${task.exception}")
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 }
--- app/src/main/java/com/dscvit/policewatch/ui/auth/VerifyOtpActivity.kt
+++ app/src/main/java/com/dscvit/policewatch/ui/auth/VerifyOtpActivity.kt
@@ -1,10 +1,22 @@
 package com.dscvit.policewatch.ui.auth
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.dscvit.policewatch.databinding.ActivityVerifyOtpBinding
+import com.dscvit.policewatch.ui.utils.showErrorSnackBar
+import com.dscvit.policewatch.ui.utils.showSuccessSnackBar
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class VerifyOtpActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "VerifyOtpActivity"
+    }
 
     private lateinit var binding: ActivityVerifyOtpBinding
 
@@ -21,5 +33,33 @@ class VerifyOtpActivity : AppCompatActivity() {
         binding.backButton.setOnClickListener {
             finish()
         }
+
+        binding.verifyButton.setOnClickListener {
+            if (binding.otpTextField.otp?.length != 6) {
+                binding.root.showErrorSnackBar("Please enter a valid OTP")
+            } else {
+                verifyOTP(binding.otpTextField.otp ?: "")
+            }
+        }
+    }
+
+    private fun verifyOTP(code: String) {
+        val credential =
+            PhoneAuthProvider.getCredential(intent.getStringExtra("verificationID") ?: "", code)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        Firebase.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    binding.root.showSuccessSnackBar("Signed In!")
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = task.result?.user
+                } else {
+                    binding.root.showErrorSnackBar("${task.exception?.message}")
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 }
